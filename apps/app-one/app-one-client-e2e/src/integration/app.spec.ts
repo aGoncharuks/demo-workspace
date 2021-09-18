562c2647459fb0fe9be56a4d5d7e506dd8f27721
Alexander Goncharuk <alexander.goncharuk@brainbase.com>
2021-09-18 15:51:12 +0300

    e2e(app-one): Fix e2e tests in app one

--- apps/app-one/app-one-client-e2e/src/integration/app.spec.ts
+++ apps/app-one/app-one-client-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('client', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to client!');
+    getGreeting().contains('Micro Frontend Demo');
   });
 });
