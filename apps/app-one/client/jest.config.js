73355a00c3fde782cf7f10210d6e4d45ed8ed1c2
Alexander Goncharuk <alexander.goncharuk@brainbase.com>
2021-09-19 15:53:54 +0300

    fix(test): fix root jest config paths

--- apps/app-one/client/jest.config.js
+++ apps/app-one/client/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'client',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
