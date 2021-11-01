30b6ed82fe92f8fe9672be90e567522e0535bbfe
Alexander Goncharuk <alexander.goncharuk@brainbase.com>
2021-11-01 14:26:22 +0200

    feat(admin-company-management): create new admin company management library

---
+++ libs/admin/company-management/src/lib/admin-company-management.spec.ts
@@ -0,0 +1,7 @@
+import { adminCompanyManagement } from './admin-company-management';
+
+describe('adminCompanyManagement', () => {
+  it('should work', () => {
+    expect(adminCompanyManagement()).toEqual('admin-company-management');
+  });
+});
---
+++ libs/admin/company-management/src/lib/admin-company-management.ts
@@ -0,0 +1,3 @@
+export function adminCompanyManagement(): string {
+  return 'admin-company-management';
+}
