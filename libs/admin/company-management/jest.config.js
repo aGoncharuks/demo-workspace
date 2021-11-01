8794cbf98661b49a5d35f943594c3ce7cf81a04f
Alexander Goncharuk <alexander.goncharuk@brainbase.com>
2021-11-01 14:05:30 +0200

    chore(admin): remove all admin apps and libs using React

--- libs/admin/company-management/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: 'admin-company-management',
-  preset: '../../../jest.preset.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/admin/company-management',
-};
